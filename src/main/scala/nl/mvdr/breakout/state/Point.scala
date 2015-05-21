e89ad3a67a3ad306d838cb4683979fecec105ce9
Martijn van de Rijdt <mvdrijdt@planet.nl>
2015-05-21 14:00:50 +0200

    Scaladoc for parameters.

--- src/main/scala/nl/mvdr/breakout/state/Point.scala
+++ src/main/scala/nl/mvdr/breakout/state/Point.scala
@@ -3,6 +3,9 @@ package nl.mvdr.breakout.state
 /**
  * A point in the two-dimensional game space.
  *
+ * @param x horizontal coordinate; 0.0 is the left border of the playing field
+ * @param y vertical coordinate; 0.0 is the top border of the playing field
+ * 
  * @author Martijn van de Rijdt
  */
 case class Point(val x: Double, y: Double) {
