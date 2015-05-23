e2f5e173a6219e2215fd42bcf4af7ce6b1555fbe
Martijn van de Rijdt <mvdrijdt@planet.nl>
2015-05-23 17:51:25 +0200

    Added Brick.

---
+++ src/main/scala/nl/mvdr/breakout/state/Brick.scala
@@ -0,0 +1,30 @@
+package nl.mvdr.breakout.state
+
+/**
+ * A brick. The object of the game is to destroy all of these.
+ *
+ * @param location location of the brick
+ * @param hitPoints the number of hit points; must be positive
+ * 
+ * @author Martijn van de Rijdt
+ */
+case class Brick(override val location: Point, val hitPoints: Int) extends GameObject(location, 40, 10) {
+  
+  require(0 < hitPoints)
+  
+  /**
+   * Constructor. Creates a brick with one hit point.
+   * 
+   * @param location location of the brick
+   */
+  def this(location: Point) = this(location, 1)
+  
+  /**
+   * Processes a hit by subtracting a hit point.
+   * 
+   * @return empty if hit points were reduced to zero, a copy of this brick with one less HP otherwise
+   */
+  def takeDamage: Option[Brick] = 
+    if (hitPoints == 1) Option.empty
+    else Option(copy(hitPoints = this.hitPoints - 1))
+}
---
+++ src/test/scala/nl/mvdr/breakout/state/BrickSpec.scala
@@ -0,0 +1,35 @@
+package nl.mvdr.breakout.state
+
+import org.scalatest._
+
+/**
+ * Test specification for {@link Brick}.
+ *
+ * @author Martijn van de Rijdt
+ */
+class BrickSpec extends FlatSpec with Matchers {
+  "A brick" should "be destroyed when its hit points are reduced to zero" in {
+    val brick = new Brick(Point(0, 0))
+    
+    brick.takeDamage should be (Option.empty)
+  }
+  
+  it should "lose a hit point when it takes damage" in {
+    val brick = Brick(Point(0, 0), 5)
+    
+    brick.takeDamage should be (Option(Brick(Point(0, 0), 4)))
+  }
+  
+  it should "not be allowed to have negative hit points" in {
+    intercept[IllegalArgumentException] {
+      val brick = Brick(Point(0, 0), -10)
+    }
+  }
+  
+  it should "not be allowed to have zero hit points" in {
+    intercept[IllegalArgumentException] {
+      val brick = Brick(Point(0, 0), 0)
+    }
+  }
+
+}
