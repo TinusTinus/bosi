f38e7ac5e7d2eab7e53ccdce67663c541aade734
Martijn van de Rijdt <mvdrijdt@planet.nl>
2015-05-24 23:06:16 +0200

    Added logback configuration and startup scripts.

---
+++ src/main/assembly/files/bosi.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: Startup script for BOSI.
+:: This script assumes that a valid javaw.exe, version 1.8 or later, is on the PATH.
+
+set PWD="%CD%"
+
+set LIB_DIR=%PWD%\lib
+
+set CONFIG_DIR=%PWD%\etc
+
+set NATIVES_DIR=%PWD%\natives
+
+set JAVA_OPTS=-Djava.library.path=%NATIVES_DIR%
+
+set JAVA=javaw
+
+start %JAVA% %JAVA_OPTS% -cp %CONFIG_DIR%;%LIB_DIR%\* nl.mvdr.breakout.Breakout
